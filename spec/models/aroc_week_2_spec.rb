fa764ba48cf9f4e23ee6ef2f5983996ae6b827d9
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-20 20:40:52 -0600

    Complete #11

--- spec/models/aroc_week_2_spec.rb
+++ spec/models/aroc_week_2_spec.rb
@@ -55,11 +55,12 @@ describe 'ActiveRecord Obstacle Course, Week 2' do
     ]
 
     # ----------------------- Using Ruby -------------------------
-    orders = Order.all.sort_by { |order| order.amount }
+    # orders = Order.all.sort_by { |order| order.amount }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
+    orders = Order.order(:amount)
     # ------------------------------------------------------------
 
     # Expectation
