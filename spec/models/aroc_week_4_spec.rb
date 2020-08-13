5563085922e08ce7a7ae93fcf3ddcbfcd25c3c63
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-13 17:39:57 -0600

    Complete #24

--- spec/models/aroc_week_4_spec.rb
+++ spec/models/aroc_week_4_spec.rb
@@ -63,13 +63,14 @@ describe 'ActiveRecord Obstacle Course, Week 4' do
     expected_result = [@order_11, @order_5]
 
     # ------------------ Inefficient Solution -------------------
-    orders = Order.where(user: @user_2)
-    order_ids = OrderItem.where(order_id: orders, item: @item_4).map(&:order_id)
-    orders = order_ids.map { |id| Order.find(id) }
+    # orders = Order.where(user: @user_2)
+    # order_ids = OrderItem.where(order_id: orders, item: @item_4).map(&:order_id)
+    # orders = order_ids.map { |id| Order.find(id) }
     # -----------------------------------------------------------
 
     # ------------------ Improved Solution ----------------------
     #  Solution goes here
+    orders = Order.where(user_id: @user_2.id).joins(:order_items).where(order_items: {item_id: @item_4})
     # -----------------------------------------------------------
 
     # Expectation
